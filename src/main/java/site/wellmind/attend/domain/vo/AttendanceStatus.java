e64c09bb401cdaeaa15a0dcd4ec5002cdb953468
tjrdbfl <tjrdbfl123@gmail.com>
2024-11-19 15:54:08 +0900

    Refactor  - change LT -> LA enum in AttendanceStatus

--- src/main/java/site/wellmind/attend/domain/vo/AttendanceStatus.java
+++ src/main/java/site/wellmind/attend/domain/vo/AttendanceStatus.java
@@ -1,6 +1,6 @@
 package site.wellmind.attend.domain.vo;
 
 public enum AttendanceStatus {
-    NA,NL,LT,EL,AB,LL,VC,OT,RT,BT
-    // NA = "정상 출근", NL = "정상 퇴근", LT = "지각", EL = "조퇴", AB = "결근", LL = "야근", VC = "휴가", OT = "외출", RT = "복귀", BT = "출장"
+    NA,NL,LA,EL,AB,LL,VC,OT,RT,BT
+    // NA = "정상 출근", NL = "정상 퇴근", LAGI = "지각", EL = "조퇴", AB = "결근", LL = "야근", VC = "휴가", OT = "외출", RT = "복귀", BT = "출장"
 }
